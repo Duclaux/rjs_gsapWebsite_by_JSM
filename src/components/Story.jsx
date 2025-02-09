24acb113b282def41cfc2d1aec12bde66375f3b4
Duclaux <tagneduclaux@gmail.com>
2025-02-09 03:16:11 +0100

    last commit about this project

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,18 +1,22 @@
 import React, { useRef } from 'react'
 import AnimatedTitle from './AnimatedTitle'
-import gsap from 'gsap'
+import gsap from "gsap"
+import Button from './Button';
 
 const Story = () => {
-  const frameRef = useRef('null')
+  const frameRef = useRef('null');
+
   const handleMouseLeave = () => {
     const element = frameRef.current;
 
-    gsap.to(element, {
-      duration:0.3,
-      rotateX : 0,
-      rotateY : 0,
-      ease: "power1.inOut"
-    });
+    if(element){
+      gsap.to(element, {
+        duration:0.3,
+        rotateX : 0,
+        rotateY : 0,
+        ease: "power1.inOut"
+      });
+    }
   }
   const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
@@ -49,19 +53,28 @@ const Story = () => {
 
           <div className="story-img-container">
             <div className="story-img-mask">
-              <div className="story-img-cotent">
+              <div className="story-img-content">
                 <img 
+                  ref={frameRef}
                   onMouseLeave={handleMouseLeave}
                   onMouseUp={handleMouseLeave}
                   onMouseEnter={handleMouseLeave}
                   onMouseMove={handleMouseMove}
-                  ref={frameRef}
                   src="/img/entrance.webp" 
                   alt="" 
                   className='object-contain'  
                 />
               </div>
             </div>
+
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-74 md:me-44 md:justify-end">
+          <div className='flex h-full w-fit flex-col items-center md:items-start'>
+            <p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start'> Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your fate amidst infinite opportunities.</p>
+          
+            <Button id="realm-button" title="discover prologue" containerClass="mt-5"/>
           </div>
         </div>
       </div>
